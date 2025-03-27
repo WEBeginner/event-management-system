873f515dc163e9e6cfaa5abe04be04bd06292378
WEBegineer <tankw618@gmail.com>
2025-03-27 16:39:26 +0800

    route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,27 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+// you may change the  routes here if you want to
+// Public routes
+Route::get('/', [EventController::class, 'index'])->name('home');
+Route::resource('events', EventController::class)->only(['index', 'show']);
+
+// Auth routes
+Route::middleware(['auth'])->group(function () {
+    // Events
+    Route::resource('events', EventController::class)->except(['index', 'show']);
+    
+    // Attendees
+    Route::post('/events/{event}/attend', [AttendeeController::class, 'store'])
+         ->name('events.attend');
+    Route::delete('/events/{event}/cancel', [AttendeeController::class, 'destroy'])
+         ->name('events.cancel');
+         
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    // Categories (Admin only)
+    Route::resource('categories', CategoryController::class)->middleware('can:admin');
+});
+
